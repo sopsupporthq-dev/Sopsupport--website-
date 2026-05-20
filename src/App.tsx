89e31adf48d6ee51c9b4660b6112538481e0ee5a
sopsupporthq-dev <sopsupporthq@gmail.com>
2026-05-20 17:55:35 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Switch, Route, Router as WouterRouter } from "wouter";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/not-found";
 import LaunchProgram from "@/pages/LaunchProgram";
